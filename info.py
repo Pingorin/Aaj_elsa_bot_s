0908734d3d257b2b29315c60ed88af0fe2e280e3
Pingorin <125970228+Pingorin@users.noreply.github.com>
2025-11-11 00:32:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -50,7 +50,9 @@ SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", "")
 SHORTENER_API2 = environ.get("SHORTENER_API2", "")
 SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", "")
 TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "0"))
-
+# (Aapki info.py file mein...)
+# (Default verification duration 24 ghante = 86400 seconds)
+VERIFY_DURATION = int(environ.get('VERIFY_DURATION', "86400")) 
 # languages search
 LANGUAGES = ["hindi", "english", "telugu", "tamil", "kannada", "malayalam"]
 
