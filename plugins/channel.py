7c503729c7b96f6ec0760b9560fb156927a747b8
Pingorin <125970228+Pingorin@users.noreply.github.com>
2025-11-19 01:05:42 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -7,7 +7,7 @@ media_filter = filters.document | filters.video
 @Client.on_message(filters.chat(CHANNELS) & media_filter)
 async def media(bot, message):
     media = getattr(message, message.media.value, None)
-    if media.mime_type in ['video/mp4', 'video/x-matroska']:  # Non .mp4 and .mkv files are skipped
+    if media.mime_type in ['video/mp4', 'video/x-matroska']:
         media.file_type = message.media.value
         media.caption = message.caption
         await save_file(media)
